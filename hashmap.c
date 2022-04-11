72a30cd501d075ea976bf517a6d1e59e6872ebac
CARLOSAGUIRRE10 <>
2022-04-11 02:45:41 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -219,7 +219,7 @@ Pair * nextMap(HashMap * map)
   {
     for(cont = 0 ; cont < map->capacity; cont++)
     {
-      if(map->buckets[aux]->key != NULL)
+      if(map->buckets[aux] != NULL)
       {
         map->current = aux;
         return map->buckets[aux];
