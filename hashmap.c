ef3664cc2c04e337499fe3d1ac58031468cc8619
CARLOSAGUIRRE10 <>
2022-04-11 02:57:52 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -215,7 +215,7 @@ Pair * nextMap(HashMap * map)
   long aux = map->current + 1;
   long cont;
 
-  if(map != NULL && map->size > 0)
+  if(map != NULL && map->size > 1)
   {
     for(cont = 0 ; cont < map->capacity; cont++)
     {
