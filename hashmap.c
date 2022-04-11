63a8dd82c7dc6e7ac6d518eae24e304e6bfc9e7b
CARLOSAGUIRRE10 <>
2022-04-11 02:12:10 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -96,7 +96,7 @@ void enlarge(HashMap * map)
 
     for(cont = 0 ; cont < map->capacity ; cont++)
     {
-      if(auxBuckets[cont] != NULL && auxBuckets[cont]->key != NULL)
+      if(auxBuckets[cont] != NULL)
       {
         insertMap(map,auxBuckets[cont]->key,auxBuckets[cont]->value);
       }
